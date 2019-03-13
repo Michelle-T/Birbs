1d59490db617470a99fcbefbfd6ca0566c23dbd4
Michelle Truong <michelletruong@knights.ucf.edu>
2019-03-13 04:43:38 -0400

    Script to for player interaction
    The player can touch whatever they want and we'll get the feedback in the Debug.log. Set the cube as "Kiwi" to see for point system setup but since the player can touch everything, we can set if else statements for what happens when they touch a certain item. Need to look into a scoreboard setup as well.

--- Birbs/Assets/Scripts/PControllerPH.cs
+++ Birbs/Assets/Scripts/PControllerPH.cs
@@ -8,16 +8,31 @@ public class PControllerPH : MonoBehaviour
 
     private Rigidbody rb;
 
+    Camera cam;
+
+    public LayerMask kiwiMask;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        cam = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+            }
+        }
     }
 
     private void FixedUpdate()
@@ -25,7 +40,7 @@ public class PControllerPH : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveHorizontal);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
     }
