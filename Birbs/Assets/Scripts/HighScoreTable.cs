11f9eddd9a73371eaf06646d5de361547bcf8f70
Michelle Truong <michelletruong@knights.ucf.edu>
2019-04-16 03:17:36 -0400

    HighScore
    HighScore Board isn't working as intended and problem can't seem to be solved, will revise in the morning or replace with another code

--- Birbs/Assets/Scripts/HighScoreTable.cs
+++ Birbs/Assets/Scripts/HighScoreTable.cs
@@ -9,6 +9,9 @@ public class HighScoreTable : MonoBehaviour
     public Transform entryTemplate;
     public List<Transform> entryTransformList;
 
+    private List<HighscoreEntry> highscoreEntryList;
+    private List<Transform> highscoreEntryTransformList;
+
     private void Awake()
     {
         //entryContainer = transform.Find("EntryContainer");
@@ -16,18 +19,75 @@ public class HighScoreTable : MonoBehaviour
 
         entryTemplate.gameObject.SetActive(false);
 
-        float templateHeight = 25f;
-        int i;
-        for (i = 0; i < 10; i++)
+        //AddHighscoreEntry(100);
+
+        highscoreEntryList = new List<HighscoreEntry>()
         {
-            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
-            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-            entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * i);
-            entryTransform.gameObject.SetActive(true);
+            new HighscoreEntry{score = 1000},
+            new HighscoreEntry{score = 2000},
+            new HighscoreEntry{score = 3000},
+            new HighscoreEntry{score = 4000},
+            new HighscoreEntry{score = 5000},
+        };
+
+     /* string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+        */
+        for (int i = 0; i < highscoreEntryList.Count; i++)
+        {
+            for (int k = i + 1; k < highscoreEntryList.Count; k++)
+            {
+                if (highscoreEntryList[k].score > highscoreEntryList[i].score)
+                {
+                    HighscoreEntry tmp = highscoreEntryList[i];
+                    highscoreEntryList[i] = highscoreEntryList[k]; ;
+                    highscoreEntryList[k] = tmp;
+                }
+            }
+        }
+
+        highscoreEntryTransformList = new List<Transform>();
+        foreach (HighscoreEntry highscoreEntry in highscoreEntryList)
+        {
+            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
         }
     }
 
-    if (highscores.highscoreEntryList[a].score > highscores.)
+    private void CreateHighscoreEntryTransform(HighscoreEntry highscoreEntry, Transform container, List<Transform> transformList)
+    {
+        float templateHeight = 20f;
+        Transform entryTransform = Instantiate(entryTemplate, container);
+        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+        entryTransform.gameObject.SetActive(true);
+
+        int score = highscoreEntry.score;
+    }
+
+    private void AddHighscoreEntry(int score)
+    {
+        HighscoreEntry highscoreEntry = new HighscoreEntry { score = score };
+
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+        highscores.highscoreEntryList.Add(highscoreEntry);
+
+        string json = JsonUtility.ToJson(highscores);
+        PlayerPrefs.SetString("highscoreTable", json);
+        PlayerPrefs.Save();
+    }
+
+    private class Highscores
+    {
+        public List<HighscoreEntry> highscoreEntryList;
+    }
+
+    [System.Serializable]
+    private class HighscoreEntry
+    {
+        public int score;
+    }
 
     // Start is called before the first frame update
     void Start()
